ca8b3730fe1a424a975d74456f454a338b1b4f46
6031 <dqas1004t@naver.com>
2024-11-26 10:43:29 +0900

    [update]_241126_스레드

---
+++ src/main/java/org/example/ClientRequestHandler.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import org.example.calculate.Calculator;
+import org.example.calculate.PositiveNumber;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ClientRequestHandler implements Runnable{
+
+    private static final Logger logger = LoggerFactory.getLogger(ClientRequestHandler.class);
+
+    private final Socket clientSocket;
+
+    public ClientRequestHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+        logger.info("ClientRequestHandler {} started", Thread.currentThread().getName());
+        try(InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()){
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+            DataOutputStream dos = new DataOutputStream(out);
+
+            HttpRequest httpRequest = new HttpRequest(br);
+
+            if(httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")){
+                QueryStrings queryStrings = httpRequest.getQueryStrings();
+
+                int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
+                String operator = queryStrings.getValue("operator");
+                int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
+
+                int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+                byte[] body = String.valueOf(result).getBytes();
+
+                HttpResponse response = new HttpResponse(dos);
+                response.response200Header("application/json", body.length);
+                response.responseBody(body);
+            }
+
+                    /*String line;
+                    while((line = br.readLine()) != ""){
+                        System.out.println(line);
+                    }*/
+        }catch (IOException e){
+            logger.error(e.getMessage(), e);
+        }
+    }
+}
--- src/main/java/org/example/CustomWebApplicationServer.java
+++ src/main/java/org/example/CustomWebApplicationServer.java
@@ -1,21 +1,17 @@
 package org.example;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.logging.Logger;
-import org.example.calculate.Calculator;
-import org.example.calculate.PositiveNumber;
 
 public class CustomWebApplicationServer {
 
     private final int port;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     private static final Logger logger = Logger.getLogger(CustomWebApplicationServer.class.getName());
 
@@ -33,32 +29,7 @@ public class CustomWebApplicationServer {
             while((clientSocket = serverSocket.accept()) != null){
                 logger.info("Custom Web Application Server client Connected");
 
-                try(InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()){
-                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-                    DataOutputStream dos = new DataOutputStream(out);
-
-                    HttpRequest httpRequest = new HttpRequest(br);
-
-                    if(httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")){
-                        QueryStrings queryStrings = httpRequest.getQueryStrings();
-
-                        int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
-                        String operator = queryStrings.getValue("operator");
-                        int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
-
-                        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
-                        byte[] body = String.valueOf(result).getBytes();
-
-                        HttpResponse response = new HttpResponse(dos);
-                        response.response200Header("application/json", body.length);
-                        response.responseBody(body);
-                    }
-
-                    /*String line;
-                    while((line = br.readLine()) != ""){
-                        System.out.println(line);
-                    }*/
-                }
+                executorService.execute(new ClientRequestHandler(clientSocket));
             }
 
         }
